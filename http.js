daf1d38cddb0a5627ab9df757fb092fde1619d4c
zhangshaoxuan <web_zsx@163.com>
2018-10-19 12:02:12 +0800

    change

--- http.js
+++ http.js
@@ -1,27 +1,27 @@
-var PORT = 3000;//
- 
+var PORT = 3030; //
+
 var http = require('http');
-var url=require('url');
-var fs=require('fs');
-var mine=require('./mine').types;//
-var path=require('path');
- 
-var server = http.createServer(function (request, response) {
+var url = require('url');
+var fs = require('fs');
+var mine = require('./mine').types; //
+var path = require('path');
+
+var server = http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname;
-    var realPath = path.join("demo-select", pathname);    //这里设置自己的文件名称;
- 
+    var realPath = path.join("demo-select", pathname); //这里设置自己的文件名称;
+
     var ext = path.extname(realPath);
     ext = ext ? ext.slice(1) : 'unknown';
-    fs.exists(realPath, function (exists) {
+    fs.exists(realPath, function(exists) {
         if (!exists) {
             response.writeHead(404, {
                 'Content-Type': 'text/plain'
             });
- 
+
             response.write("This request URL " + pathname + " was not found on this server.");
             response.end();
         } else {
-            fs.readFile(realPath, "binary", function (err, file) {
+            fs.readFile(realPath, "binary", function(err, file) {
                 if (err) {
                     response.writeHead(500, {
                         'Content-Type': 'text/plain'
@@ -40,4 +40,4 @@ var server = http.createServer(function (request, response) {
     });
 });
 server.listen(PORT);
-console.log("Server runing at port: " + PORT + ". url: http://localhost:3000/index.html");
+console.log("Server runing at port: " + PORT + ". url: http://localhost:3030/index.html");
